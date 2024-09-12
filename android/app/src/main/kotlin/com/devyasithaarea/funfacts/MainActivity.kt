5d2c72f55277d363ddc7c1a514ae6328487b26f0
yasithabhanuka <yasithabhanukac@gmail.com>
2024-09-13 03:56:13 +0530

    Create the basic layout for the main_screen and setting_screen

---
+++ android/app/src/main/kotlin/com/devyasithaarea/funfacts/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.devyasithaarea.funfacts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:funfacts/screens/settings_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Fun Facts"),
+        actions: [
+          InkWell(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const SettingsScreen()));
+            },
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Icon(Icons.settings),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
